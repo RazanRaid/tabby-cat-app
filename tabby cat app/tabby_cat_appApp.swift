1bd4a7a6cf331735a816fb6036184d76dae0a5a3
Razan Raid <razanraid990@gmail.com>
2023-08-27 14:24:25 +0400

    Initial Commit

---
+++ tabby cat app/tabby_cat_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  tabby_cat_appApp.swift
+//  tabby cat app
+//
+//  Created by Razan Raid on 27/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct tabby_cat_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
