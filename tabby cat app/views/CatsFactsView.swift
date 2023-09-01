b3aeea53d669e1806005c81033bdf53f0643b07a
Razan Raid <razanraid990@gmail.com>
2023-09-01 22:43:54 +0400

    cats facts view refresh button

--- tabby cat app/views/CatsFactsView.swift
+++ tabby cat app/views/CatsFactsView.swift
@@ -15,10 +15,23 @@ struct CatsFactsView: View {
         VStack{
             if let catFact = catFactsManager.catFact {
                 Text(catFact.fact)
-                    .padding()
+                    .padding(17)
+                    .font(.title)
+                    
             }else{
                 ProgressView()
                     .progressViewStyle(.circular)
+            }
+            Button {
+                catFactsManager.catFact=nil
+                catFactsManager.getCatFact()
+            }label: {
+                Text("next fact")
+                    .padding(10)
+                    .foregroundColor(.white)
+                    .background(.brown)
+                    .cornerRadius(10)
+                  
             }
         }
             .onAppear{
